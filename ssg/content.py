2b59d9a57af833a69da7b375b1f698f3fe1b7c17
Zaratools <61806515+Zaratools@users.noreply.github.com>
2022-01-19 09:38:35 -0500

    Module 4: The Content Class

---
+++ ssg/content.py
@@ -0,0 +1,51 @@
+import re
+from yaml import load
+from yaml import FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimiter = "^(?:-|\+){3}\s*$";
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    def load(self, cls, string):
+        (_, fm, content) = self.__regex.split(string, 2)
+        load(fm, FullLoader)
+        return cls(metadata, content)
+
+
+class Content:
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+        @property
+        def body(self):
+            return self.data["content"]
+
+        @property
+        def type(self):
+            if "type" in self.data:
+                return self.data["type"]
+            else:
+                return None
+
+        @type.setter
+        def type(self, value):
+            self.data["type"] = value
+
+        def __getitem__(self, key):
+            return self.data[key]
+
+        def __iter__(self):
+            return self.data.iterator
+
+        def __len__(self):
+            return self.data.length
+
+        def __repr__(self):
+            data = {}
+            for (key, value) in self.data.items():
+                if key != "content":
+                    data[key] = value
+            return str(data)
